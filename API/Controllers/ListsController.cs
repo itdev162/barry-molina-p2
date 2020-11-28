2e9f1aebe25d9558307b0290f557004d25e04964
Barry Molina <bazzaboy@gmail.com>
2020-11-28 10:07:30 -0600

    Update and delete endpoints

--- API/Controllers/ListsController.cs
+++ API/Controllers/ListsController.cs
@@ -25,13 +25,25 @@ namespace API.Controllers
         }
 
         /// <summary>
+        /// GET api/lists
+        /// </summary>
+        /// <returns>All lists associated with the authorized user</returns>
+        [Authorize]
+        [HttpGet]
+        public ActionResult<List<List>> GetLists()
+        {
+            var user = (User)HttpContext.Items["User"];
+            return _context.Lists.Where(l => l.User == user.Id).ToList();
+        }
+
+        /// <summary>
         /// POST api/lists
         /// </summary>
         /// <param name="list">The list to create</param>
         /// <returns>A new list</returns>
         [Authorize]
         [HttpPost]
-        public ActionResult Create([FromBody] List list)
+        public ActionResult CreateList([FromBody] List list)
         {
             var user = (User)HttpContext.Items["User"];
             var newList = new List
@@ -59,7 +71,7 @@ namespace API.Controllers
         /// <returns>A new list item</returns>
         [Authorize]
         [HttpPost("{id}")]
-        public ActionResult Create([FromRoute] Guid id, [FromBody] ListItem item)
+        public ActionResult CreateItem([FromRoute] Guid id, [FromBody] ListItem item)
         {
             var user = (User)HttpContext.Items["User"];
             var list = _context.Lists.Find(id);
@@ -84,20 +96,91 @@ namespace API.Controllers
         }
 
         /// <summary>
-        /// GET api/lists
+        /// PUT api/lists/{id}
         /// </summary>
-        /// <returns>A list of lists</returns>
+        /// <param name="id">The list to update</param>
+        /// <param name="newList">The new list info</param>
+        /// <returns>An updated list</returns>
         [Authorize]
-        [HttpGet]
-        public ActionResult<List<List>> GetLists()
+        [HttpPut("{id}")]
+        public ActionResult<List> UpdateList([FromRoute] Guid id, [FromBody] List newList)
         {
             var user = (User)HttpContext.Items["User"];
-            return _context.Lists.Where(l => l.User == user.Id).ToList();
+            var list = _context.Lists.Find(id);
+
+            if (list == null)
+            {
+                throw new Exception("Could not find list");
+            }
+
+            if (list.User != user.Id )
+            {
+                throw new Exception("User not authorized");
+            }
+
+            list.Title = newList.Title != null ? newList.Title : list.Title;
+
+            var success = _context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(list);
+            }
+
+            throw new Exception("Error updating list");
         }
 
+        /// <summary>
+        /// PUT api/lists/{listId}/{itemId}
+        /// </summary>
+        /// <param name="listId">The containing list</param>
+        /// <param name="itemId">The item to update</param>
+        /// <param name="newItem">The new item info</param>
+        /// <returns>An updated item</returns>
+        [Authorize]
+        [HttpPut("{listId}/{itemId}")]
+        public ActionResult<ListItem> UpdateItem([FromRoute] Guid listId, [FromRoute] Guid itemId, [FromBody] ListItem newItem)
+        {
+            var user = (User)HttpContext.Items["User"];
+            var list = _context.Lists.Find(listId);
+
+            if (list == null)
+            {
+                throw new Exception("Could not find list");
+            }
+
+            if (list.User != user.Id )
+            {
+                throw new Exception("User not authorized");
+            }
+
+            var item = list.Items.FirstOrDefault(i => i._id == itemId);
+
+            if (item == null)
+            {
+                throw new Exception("Could not find item");
+            }
+
+            item.Desc = newItem.Desc != null ? newItem.Desc : item.Desc;
+
+            var success = _context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(item);
+            }
+
+            throw new Exception("Error updating list");
+        }
+
+        /// <summary>
+        /// DELETE api/lists/{id}
+        /// </summary>
+        /// <param name="id">The list to delete</param>
+        /// <returns>True if deletion was successful</returns>
         [Authorize]
         [HttpDelete("{id}")]
-        public ActionResult<bool> Delete(Guid id)
+        public ActionResult<bool> DeleteList(Guid id)
         {
             var user = (User)HttpContext.Items["User"];
             var list = _context.Lists.Find(id);
@@ -113,6 +196,48 @@ namespace API.Controllers
             }
 
             _context.Remove(list);
+
+            var success = _context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return true;
+            }
+
+            throw new Exception("Error deleting list");
+        }
+
+        /// <summary>
+        /// DELETE api/lists/{listId}/{itemId}
+        /// </summary>
+        /// <param name="listId">The list to delete from</param>
+        /// <param name="itemId">The item to delete</param>
+        /// <returns>True if deletion was successful</returns>
+        [Authorize]
+        [HttpDelete("{listId}/{itemId}")]
+        public ActionResult<bool> DeleteItem(Guid listId, Guid itemId)
+        {
+            var user = (User)HttpContext.Items["User"];
+            var list = _context.Lists.Find(listId);
+
+            if (list == null)
+            {
+                throw new Exception("Could not find list");
+            }
+
+            if (list.User != user.Id )
+            {
+                throw new Exception("User not authorized");
+            }
+
+            var item = list.Items.FirstOrDefault(i => i._id == itemId);
+
+            if (item == null)
+            {
+                throw new Exception("Could not find item");
+            }
+
+            _context.Remove(item);
 
             var success = _context.SaveChanges() > 0;
 
